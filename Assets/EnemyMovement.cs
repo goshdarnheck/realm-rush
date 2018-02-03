da4f717be12d25ce1e7a33c5e9392014fadb81e5
Matthew Rapati <mattrapati@gmail.com>
2018-02-03 13:47:33 -0500

    Generate name of super cubes from position, added block script to super cubes, added more blocks and enemy etc. etc.

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] List<Block> path;
+
+	// Use this for initialization
+	void Start () {
+        PrintAllWaypoints();
+	}
+
+    void PrintAllWaypoints() {
+        foreach (Block waypoint in path) {
+            print(waypoint.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
