102feb4d80f38ce808e818f1655a2d7ca45721a7
Matthew Rapati <mattrapati@gmail.com>
2018-03-03 15:58:29 -0500

    Lighting changes.

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	[SerializeField] Transform objectToPan;
+	[SerializeField] Transform targetEnemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LookAtEnemy();
+	}
+
+	void LookAtEnemy() {
+		objectToPan.LookAt(targetEnemy);
+	}
+}
