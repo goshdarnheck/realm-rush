fca3d46fc2ab13e49dcc10258d859b82bcb741b9
Matthew Rapati <mattrapati@gmail.com>
2018-03-04 10:46:51 -0500

    Challenge: towers shoot, enemies have health, enemies die and play FX when destroyed

---
+++ Assets/Scripts/EnemyEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEntity : MonoBehaviour {
+
+	[SerializeField] int health = 10;
+	[SerializeField] GameObject deathFX;
+	[SerializeField] Transform parentForFX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnParticleCollision(GameObject other) {
+		health--;
+		print(health);
+		if (health <= 0) {
+			GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+			fx.transform.parent = parentForFX;
+        	Destroy(gameObject);
+		}
+    }
+}
